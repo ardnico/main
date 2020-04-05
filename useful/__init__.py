5d1ebfcddc6d34a754ca3ea378db7784bc9dd3ef
ardnico <leaf.sun2@gmail.com>
2020-04-06 00:07:52 +0900

    rewrite init

--- useful/__init__.py
+++ useful/__init__.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+import sys
+import pathlib
+# base.pyのあるディレクトリの絶対パスを取得
+current_dir = pathlib.Path(__file__).resolve().parent
+# モジュールのあるパスを追加
+sys.path.append( str(current_dir) + '/../' )
 
 hard_dependencies = ("chardet","pandas")
 missing_dependencies = []
@@ -8,6 +14,7 @@ for dependency in hard_dependencies:
         __import__(dependency)
     except ImportError as e:
         missing_dependencies.append("{0}: {1}".format(dependency, str(e)))
+
 
 from useful.read_data import *
 from useful.loglotate import *
