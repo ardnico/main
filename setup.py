96f41690f1fe3369154b632d8b60d280b23ef0ea
ardnico <leaf.sun2@gmail.com>
2020-04-06 00:59:33 +0900

    rewrite setup

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 import sys
 import os
 import os.path
@@ -12,9 +16,21 @@ setup(
     version='1.0.0',
     description='usuful method for python3',
     install_requires=["pandas", "chardet","selenium"],
+    packages=["read_data","UseSel","loglotate"],
     long_description='readme.md',
     author='Masaru Abe',
     author_email='leaf.sun2@gmail.com',
     url='https://qiita.com/mabe',
-    license='MIT license'
+    license='MIT license',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': ['read_data = usesel.read_data:main',
+                            'UseSel = useful.UseSel:main',
+                            'loglotate = useful.loglotate:main']
+    },
+    python_requires='>=3.5',
 )
