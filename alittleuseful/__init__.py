63492cb53bc2acd9dca9804a57447a09da1d75b5
ardnico <leaf.sun2@gmail.com>
2020-08-02 09:50:56 +0900

    insert 'try'

--- alittleuseful/__init__.py
+++ alittleuseful/__init__.py
@@ -3,7 +3,19 @@ __author__ = 'Masaru Abe'
 __lisence__ = 'MIT'
 __version__ = '1.0.0'
 
-from .read_data import read_data
-from .ASE_files import ASE_files
-from .loglotate import loglotate
-from .UseSel import UseSel
+try:
+    from .read_data import read_data
+except:
+    pass
+try:
+    from .loglotate import loglotate
+except:
+    pass
+try:
+    from .UseSel import UseSel
+except:
+    pass
+try:
+    from .ASE_files import ASE_files
+except:
+    pass
